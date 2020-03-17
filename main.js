0db15e21f00ac6f8fca00a2d021378bfb19e612c
Marina108 <60389528+Marina108@users.noreply.github.com>
2020-03-17 14:33:20 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+var day = prompt('Weekday')
+
+switch(day) {
+    case 'Monday':
+    case 'Tuesday':
+    case 'Wednesday':
+    case 'Thursday':
+    case 'Friday':
+        alert('Working day');
+    break;
+    case 'Saturday':
+    case 'Sunday':
+        alert('Weekend');
+    break;
+    default :
+        alert('Can not define, please try another value')
+}
